4dda88f64e53e438b8a40c4b94e2058848d85d51
Miroslav <placeformovies@gmail.com>
2019-05-16 02:02:01 +0200

    Get React Ready

---
+++ front-end/react-client/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <div className="App">Hello wp project!</div>;
+}
+
+export default App;
